b49e8d92d146e0bf393257b4dbdc97f0c55f04fc
David Koerner <dk402538@googlemail.com>
2011-02-20 18:46:17 +0000

    update base and main

--- src/main.cpp
+++ src/main.cpp
@@ -8,31 +8,7 @@
 #include <string>
 #include <iostream>
 
-#include <math/Vec3f.h>
-#include <ui/GLWindow.h>
-
-//#ifdef WINDOWS
-//#include <windows.h>
-//#endif
-//#include <GL/gl.h>
-/*
-#include <GL/gl.h>
-#include <GL/glx.h>
-#include <GL/glut.h>
-*/
-
-
-
-
-#define XRES 640
-#define YRES 480
-
-
-
-
-
-
-
+#include <ui/GLViewer.h>
 
 
 
@@ -40,7 +16,7 @@
 
 int main(int argc, char ** argv)
 {
-	base::GLWindow window( 800, 600, "test" ); 
+	base::GLViewer window( 800, 600, "test" ); 
 	window.show();
 	base::Application app;
 	return app.exec();
