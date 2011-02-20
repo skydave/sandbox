74169af02be99919f5f28448cc823d9fe782a580
David Koerner <dk402538@googlemail.com>
2011-02-20 21:10:39 +0000

    ...

--- src/main.cpp
+++ src/main.cpp
@@ -9,14 +9,39 @@
 #include <iostream>
 
 #include <ui/GLViewer.h>
+#include <gltools/gl.h>
+#include <gltools/misc.h>
 
+
+void render( base::Camera *cam )
+{
+	glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+	glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glMatrixMode( GL_PROJECTION );
+	glPushMatrix();
+	glLoadMatrixf( cam->m_projectionMatrix.ma );
+
+	glMatrixMode( GL_MODELVIEW );
+	glPushMatrix();
+	glLoadMatrixf( (GLfloat *)cam->m_viewMatrix.ma );
+
+	// draw scene
+	base::drawGrid();
+
+	glMatrixMode( GL_PROJECTION );
+	glPopMatrix();
+
+	glMatrixMode( GL_MODELVIEW );
+	glPopMatrix();
+}
 
 
 
 
 int main(int argc, char ** argv)
 {
-	base::GLViewer window( 800, 600, "test" ); 
+	base::GLViewer window( 800, 600, "test", render ); 
 	window.show();
 	base::Application app;
 	return app.exec();
