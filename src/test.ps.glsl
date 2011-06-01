8d6cb60774a7593c3467c50ac1922106f684f60b
David Koerner <david-k@quirky.mpc.local>
2011-06-01 20:16:22 +0100

    continued to work on flowtexture shader

--- src/test.ps.glsl
+++ src/test.ps.glsl
@@ -9,21 +9,28 @@ uniform sampler2D tex;
 void main()
 {
 	float interval = 5.0;
+	float speed = 0.1;
 	// comes from flow texture
 	vec2 vel = vec2( 1.0, 2.0 );
 
-	float ttime = mod(time, interval);
-	float t = ttime/interval; // get weight (0-1)
+	float ttime0 = mod(time, interval);
+	float ttime1 = mod(time+interval*0.5, interval);
+	float t0 = ttime0/interval; // get weight (0-1)
 
-	if( t > 0.5)
-		t = 1.0 - t;
-	t = 2.0*t;
+
+	if( t0 > 0.5)
+	{
+		t0 = 1.0 - t0;
+	}
+	t0 = 2.0*t0;
 	
-	float t2 = 1.0 - t;
+	float t1 = 1.0 - t0;
 
-	vec2 flowuv = uv + (ttime-interval*0.5)*0.1*vel;
+	vec2 flowuv0 = uv + (ttime0-interval*0.5)*speed*vel;
+	vec2 flowuv1 = uv + (ttime1-interval*0.5)*speed*vel;
 
-	float val = texture2D(tex,flowuv).x;
-	val = val*t;
+	float val0 = texture2D(tex,flowuv0).x;
+	float val1 = texture2D(tex,flowuv1).x;
+	float val = val0*t0 + val1*t1;
 	gl_FragData[0] = vec4(val, val, val, 1.0);
 }
