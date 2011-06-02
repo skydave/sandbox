beaa24d70f874e6c71fdf9b0a2a7b845306e9ab1
David Koerner <dk402538@googlemail.com>
2011-06-02 21:13:00 +0100

    testing ps

--- src/test.ps.glsl
+++ src/test.ps.glsl
@@ -6,25 +6,25 @@ vec3 turb2d( vec2 p, int numIterations );
 uniform float time;
 uniform sampler2D tex;
 
+//vec3 flowTexture( in sampler2D tex, in sampler2D flow, in sampler2D noise, in float interval, int float speed,  )
+
 void main()
 {
+
 	float interval = 0.5;
-	float speed = 0.1;
+	float speed = 0.4;
 	// comes from flow texture
 	//vec2 vel = vec2( 1.0, 2.0 );
-	vec2 vel = vec2( sin(uv.y*5), cos(uv.x*7) );
+	vec2 vel = vec2( sin(uv.y*5.0), cos(uv.x*7.0) );
 
-	uv = uv*2.0;
 
-	vec2 uv0 = uv;
-	vec2 uv1 = uv + vec2( 0.764, 0.214 );
+	vec2 uv0 = uv*2.0;
+	vec2 uv1 = uv*2.0 + vec2( 0.764, 0.214 );
 
-	float v = noise2d(uv*1.25);
+	float v = noise2d(uv*1.25).x;
 
-	time = time + v;
-
-	float ttime0 = mod(time, interval);
-	float ttime1 = mod(time+interval*0.5, interval);
+	float ttime0 = mod(time + v, interval);
+	float ttime1 = mod(time + v + interval*0.5, interval);
 	float t0 = ttime0/interval; // get weight (0-1)
 
 
@@ -39,9 +39,35 @@ void main()
 	vec2 flowuv0 = uv0 + (ttime0-interval*0.5)*speed*vel;
 	vec2 flowuv1 = uv1 + (ttime1-interval*0.5)*speed*vel;
 
-	vec3 val0 = texture2D(tex,flowuv0);
-	vec3 val1 = texture2D(tex,flowuv1);
+	vec3 val0 = texture2D(tex,flowuv0).xyz;
+	vec3 val1 = texture2D(tex,flowuv1).xyz;
 	vec3 val = val0*t0 + val1*t1;
 	//vec3 val = vec3(v);
 	gl_FragData[0] = vec4(val, 1.0);
+
+/*
+	float HalfCycle = 0.5;
+	float ttime = mod(time, HalfCycle*2.0 );
+	float FlowMapOffset0 = ttime;
+	float FlowMapOffset1 = ttime + HalfCycle;
+	float TexScale = 2.0;
+
+
+	vec2 vel = vec2( sin(uv.y*5.0), cos(uv.x*7.0) );
+	float cycleOffset = noise2d(uv*1.25).x;
+	
+	float phase0 = cycleOffset * .5 + FlowMapOffset0;
+	float phase1 = cycleOffset * .5 + FlowMapOffset1;
+
+	// Sample texture
+	vec3 texT0 = texture2D(tex, ( uv * TexScale ) + vel * phase0 ).xyz;
+	vec3 texT1 = texture2D(tex, ( uv * TexScale ) + vel * phase1 ).xyz;
+
+	float flowLerp = ( abs( HalfCycle - FlowMapOffset0 ) / HalfCycle );
+	vec3 color = mix( texT0, texT1, flowLerp );
+	//vec3 color = texT1;
+
+
+	gl_FragData[0] = vec4(color, 1.0);
+*/
 }
