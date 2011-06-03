bbd92625f2d44363f9dde20cf576276bf7f226ed
David Koerner <david-k@moving-picture.com>
2011-06-03 18:58:25 +0100

    refactored pixelshader a bit

--- src/test.ps.glsl
+++ src/test.ps.glsl
@@ -6,25 +6,16 @@ vec3 turb2d( vec2 p, int numIterations );
 uniform float time;
 uniform sampler2D tex;
 
-//vec3 flowTexture( in sampler2D tex, in sampler2D flow, in sampler2D noise, in float interval, int float speed,  )
-
-void main()
+vec4 flowTexture( in sampler2D tex, in vec2 flow, in vec2 uv, in float time, in float noiseScale, in float interval, in float speed  )
 {
+	float halfInterval = interval*0.5;
+	vec2 uv0 = uv;
+	vec2 uv1 = uv + vec2( 0.764, 0.214 );
 
-	float interval = 0.5;
-	float speed = 0.4;
-	// comes from flow texture
-	//vec2 vel = vec2( 1.0, 2.0 );
-	vec2 vel = vec2( sin(uv.y*5.0), cos(uv.x*7.0) );
-
-
-	vec2 uv0 = uv*2.0;
-	vec2 uv1 = uv*2.0 + vec2( 0.764, 0.214 );
-
-	float v = noise2d(uv*1.25).x;
+	float v = noise2d(uv*noiseScale).x;
 
 	float ttime0 = mod(time + v, interval);
-	float ttime1 = mod(time + v + interval*0.5, interval);
+	float ttime1 = mod(time + v + halfInterval, interval);
 	float t0 = ttime0/interval; // get weight (0-1)
 
 
@@ -33,19 +24,26 @@ void main()
 		t0 = 1.0 - t0;
 	}
 	t0 = 2.0*t0;
-	
-	float t1 = 1.0 - t0;
 
-	vec2 flowuv0 = uv0 + (ttime0-interval*0.5)*speed*vel;
-	vec2 flowuv1 = uv1 + (ttime1-interval*0.5)*speed*vel;
 
-	vec3 val0 = texture2D(tex,flowuv0).xyz;
-	vec3 val1 = texture2D(tex,flowuv1).xyz;
-	vec3 val = val0*t0 + val1*t1;
-	//vec3 val = vec3(v);
-	gl_FragData[0] = vec4(val, 1.0);
+	vec2 flowuv0 = uv0 + (ttime0-halfInterval)*speed*flow;
+	vec2 flowuv1 = uv1 + (ttime1-halfInterval)*speed*flow;
+
+	vec4 val0 = texture2D(tex,flowuv0);
+	vec4 val1 = texture2D(tex,flowuv1);
+	vec4 val = val0*t0 + val1*(1.0 - t0);
+
+	return val;
+}
+
+void main()
+{
+	vec4 val = flowTexture( tex, vec2( sin(uv.y*5.0), cos(uv.x*7.0) ), uv*2.0, time, 1.0, 0.5, 0.4 );
+	gl_FragData[0] = vec4(val.xyz, 1.0);
+
 
 /*
+	// code from graphicsrunner blog
 	float HalfCycle = 0.5;
 	float ttime = mod(time, HalfCycle*2.0 );
 	float FlowMapOffset0 = ttime;
